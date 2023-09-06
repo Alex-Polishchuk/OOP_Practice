8683fd5e59fd5543cec08231bb4baa239ee1bc75
Alex-Polishchuk <alexpoli147@gmail.com>
2023-09-06 20:54:25 +0100

    ade

--- bank_oop.py
+++ bank_oop.py
@@ -14,8 +14,18 @@ class Bank:
             self.holding = self.holding + val
         else:
             print("Error, cannot deposit")
+
+    def withdrawl(self, amount_withdrawn):
+        if isinstance(amount_withdrawn, int) and amount_withdrawn > 0 and self.holding >= amount_withdrawn:
+            self.holding = self.holding - amount_withdrawn
+            print(f'Withdrawn {amount_withdrawn} pounds. New balance {self.holding} pounds')
+            
+        else:
+            print("Error, could not wtihdraw")
+
         
 a1 = Bank("alex", 500)
 a1.bal_check()
-a1.deposit()
+a1.deposit(250)
 a1.bal_check()
+a1.withdrawl(400)
