4ea175655a2def3d3d777e9a1ef3379fe27fb623
Alex-Polishchuk <alexpoli147@gmail.com>
2023-09-06 21:01:09 +0100

    added withdrawl functionality

--- bank_oop.py
+++ bank_oop.py
@@ -15,7 +15,18 @@ class Bank:
         else:
             print("Error, cannot deposit")
         
+    def withdrawl(self, val):
+        if isinstance(val, int) and val > 0 and val <= self.holding:
+            self.holding = self.holding - val
+
+            print(f'You have withdrawn {val}. New balance is {self.holding}')
+
+        else:
+            print("Error")
+
 a1 = Bank("alex", 500)
 a1.bal_check()
-a1.deposit()
+a1.deposit(250)
+a1.bal_check()
+a1.withdrawl(400)
 a1.bal_check()
