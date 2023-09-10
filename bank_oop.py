0bfa70ffd558cdc799b2961c53dcbf23d97f88ec
Alex-Polishchuk <alexpoli147@gmail.com>
2023-09-10 18:11:44 +0100

    added parent class

--- bank_oop.py
+++ bank_oop.py
@@ -1,10 +1,25 @@
 
-class Bank:
 
-    def __init__ (self, name, holding):
+class User:
+    
+    def __init__ (self, first_name, last_name, birth_day):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_day = birth_day
 
-        self.name = name
+    def person_details(self):
+        print("Here are your details")
+        print(f"First name: {self.first_name}")
+        print(f"Last name: {self.last_name}")
+        print(f"Birthday: {self.birth_day}")
+
+class Bank(User):
+
+    def __init__ (self, holding, first_name, last_name, birth_day):
+
         self.holding = holding
+        super().__init__(first_name, last_name, birth_day)
+    
 
     def bal_check(self):
         print(self.holding)
@@ -24,9 +39,7 @@ class Bank:
         else:
             print("Error")
 
-a1 = Bank("alex", 500)
-a1.bal_check()
-a1.deposit(250)
-a1.bal_check()
-a1.withdrawl(400)
-a1.bal_check()
+alex_t = Bank(600, "Alex", "Tkachenko", "16/03/2000")
+alex_t.person_details()
+
+alex_t.bal_check()
