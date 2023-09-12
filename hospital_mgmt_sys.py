d4c4893004a36fbd13236d689eb9894c4d8ee854
Alex-Polishchuk <alexpoli147@gmail.com>
2023-09-12 21:27:59 +0100

    added class inheritance functionality

--- hospital_mgmt_sys.py
+++ hospital_mgmt_sys.py
@@ -5,17 +5,27 @@
 
 class Hosipital():
 
-    def __init__(self, name, capacity):
-        self.name = name
-        self.capacity = capacity
+    def __init__(self, hspt_name, hspt_cap):
+        self.hspt_name = hspt_name
+        self.hspt_cap = hspt_cap
 
     def hospital_attr(self):
-        print(f"{self.name} has a capacity of {self.capacity}")
+        print(f"{self.hspt_name} has a capacity of {self.hspt_cap}")
 
 class Department(Hosipital):
 
-    def __init__(self, dept_name, dept_capacity):
+    def __init__(self, dept_name, dept_cap, hspt_name, hspt_cap):
         self.dept_name = dept_name
-        self.dept_capacity = dept_capacity
+        self.dept_cap = dept_cap
 
+        super().__init__(hspt_name, hspt_cap)
+
+class Patient(Department):
     
+    def __init__(self,fname, lname,status, dept_name, dept_cap, hspt_name, hspt_cap):
+        self.fname = fname
+        self.lname = lname
+        self.status = status
+
+        super().__init__(dept_name, dept_cap, hspt_name, hspt_cap)
+
