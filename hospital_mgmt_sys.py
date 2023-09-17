29f26abc88c61f233ef0e67e2f40bc3500c43ea9
Alex-Polishchuk <alexpoli147@gmail.com>
2023-09-17 21:12:27 +0100

    restructred og code

--- hospital_mgmt_sys.py
+++ hospital_mgmt_sys.py
@@ -2,49 +2,32 @@
 #First goal: track who is coming in and out of hospital (manage appointment)
 #Second goal: manage doctors and nurses
 #Thrid goal: manage patient
-class Hosipital():
+class Staff():
 
-    def __init__(self, hspt_name, hspt_cap):
-        self.hspt_name = hspt_name
-        self.hspt_cap = hspt_cap
-
-    def hospital_attr(self):
-        print(f"{self.hspt_name} has a capacity of {self.hspt_cap}")
-
-class Department(Hosipital):
-
-    def __init__(self, dept_name, dept_cap, hspt_name, hspt_cap):
-        self.dept_name = dept_name
-        self.dept_cap = dept_cap
-
-        #inherit attributes from parent class
-        super().__init__(hspt_name, hspt_cap)
-
-    def capacity_change(self, value, dept_cap):
-
-        if isinstance(value, int):
-            self.dept_capt = self.dept_cap + value
-
-        else:
-            print("Error")
-
-
-class Patient(Department):
-    
-    def __init__(self,fname, lname,status, dept_name, dept_cap, hspt_name, hspt_cap):
+    def __init__(self, fname, lname, role, department):
         self.fname = fname
         self.lname = lname
-        self.status = status
+        self.role = role
+        self.department = department
 
-        #inherit attributes from parent class 
-        super().__init__(dept_name, dept_cap, hspt_name, hspt_cap)
+    def dept_staff():
+        #List through all staff of a particular department
+        pass
 
-hosp1 = Hosipital("QMUL", 200)
-radiology = Department("Radiology", 40, "QMUL", 200)
+class Patient():
 
-print(radiology.hspt_cap)
+    def __init__(self, fname, lname, department):
+        pass
 
-#AlexT = Patient("Alex", "Tkachenko", "Sick", radiology)
+
+class Appointment():
+
+    def __init__(self, time, date, department):
+
+        self.time = time
+        self.date = date
+        self.department = department
+
 
 #by changing function in parent class, it doesn't affect the parent class (we wont have to worry about breaking the parent class)
 
