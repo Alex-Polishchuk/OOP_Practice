67c0f087d2fadf6203229bc19b41a607f07bd20b
Alex-Polishchuk <alexpoli147@gmail.com>
2023-09-17 21:24:06 +0100

    add SQL connectivity

--- hospital_mgmt_sys.py
+++ hospital_mgmt_sys.py
@@ -2,6 +2,15 @@
 #First goal: track who is coming in and out of hospital (manage appointment)
 #Second goal: manage doctors and nurses
 #Thrid goal: manage patient
+import sqlite3
+
+#Creating connection to sql database
+conn = sqlite3.connect('staff_detail.db')
+cursor = conn.cursor()
+
+#WRITE CHAGNES HERE
+conn.commit()
+conn.close()
 class Staff():
 
     def __init__(self, fname, lname, role, department):
@@ -10,8 +19,8 @@ class Staff():
         self.role = role
         self.department = department
 
-    def dept_staff():
-        #List through all staff of a particular department
+    def add_staff(self, fname, lname, role, department):
+        #takes all the above and adds it to SQL db 
         pass
 
 class Patient():
@@ -27,7 +36,4 @@ class Appointment():
         self.time = time
         self.date = date
         self.department = department
-
-
-#by changing function in parent class, it doesn't affect the parent class (we wont have to worry about breaking the parent class)
 
