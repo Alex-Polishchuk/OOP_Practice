72f75fea2de9372e41f2a363afc0ca72faa7db52
Alex-Polishchuk <alexpoli147@gmail.com>
2023-12-24 13:49:07 +0000

    adding delete function, still being built

--- hospital_mgmt_sys.py
+++ hospital_mgmt_sys.py
@@ -5,8 +5,6 @@ import csv
 patient_data = pd.read_csv("patient.csv")
 staff_data = pd.read_csv("staff.csv")
 
-print(patient_data)
-print(staff_data)
 class Staff():
     
     #creating a staff class
@@ -15,8 +13,8 @@ class Staff():
         self.role = role
         self.department = department
 
-    def add_staff(self, name, role, department):
-        #function which adds staff to file containing data
+    def add_staff(self):
+        #function which adds staff to file containing staff data
 
         with open ('staff.csv', mode='a', newline='\n') as file:
             
@@ -24,13 +22,34 @@ class Staff():
             csv_write = csv.writer(file)
 
             #append the data
-            data_to_append = [name, role, department]
+            data_to_append = [self.name, self.role, self.department]
             csv_write.writerow(data_to_append)
-        
+
+        #when exiting with function, csv file automatically closed
         print("Data appended succesfully")
 
-    def remove_staff(self, fname, lname, role, department):
-        pass
+    def remove_staff(self):
+        #function to remove particular staff member based on id
+        staff_ID_input = str(input("Please input the staff ID number you wish to remove: "))
+        match_ID = False
+            
+        with open('staff.csv', mode='r') as file:
+            
+            reader_obj = csv.reader(file)
+
+            for row in reader_obj:
+                iterr_ID = (row[3])
+            
+                if staff_ID_input == iterr_ID:
+                    print(iterr_ID, staff_ID_input)
+                    print(type(iterr_ID))
+                    print(type(staff_ID_input))
+                    match_ID = True
+                    print("Match Found")
+                    break
+                else:
+                    print("No match found")
+
 
 class Patient():
 
@@ -38,9 +57,18 @@ class Patient():
         self.name = name
         self.id = id
 
-    def add_patient (self, fname, lname, id):
-        new_data = {'ID': id, 'First_name': fname, 'Last_name':lname }
-        patient_data.loc[len(patient_data)] = new_data
+    def add_patient (self):
+        
+        with open ('patient.csv', mode='a', newline='\n') as file:
+
+            #creating a csv writer
+            csv_write = csv.writer(file)
+
+            #specify which data is to be appeneded and append it
+            patient_data_append = [self.name, self.id]
+            csv_write.writerow(patient_data_append)
+
+            print('Patient data succesfully appended')
 
     def remove_patient(self, fname, lname, deaprtment):
         pass
@@ -52,15 +80,10 @@ class Appointment():
         self.date = date
         self.staff_ID = staff_ID
         self.patient_ID = patient_ID
-        
-
 
     def book_appt(self, time, date, department, doctor):
         pass
 
 #new_P = Patient('Alex', 'Tkachenko', 1000)
 #new_P.add_patient('Alex', 'Tkachenko', 1000)
-new_staff = Staff('Brando Tye', 'Physiotherapist', 1004)
-new_staff.add_staff('Brando Tye', 'Physio', 1004)
-
-print(staff_data)
+Staff('Brando Tye', 'Physiotherapist', 1004).remove_staff()
